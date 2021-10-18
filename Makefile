d7b174d792523045533d5602fbbf25c9799ac621
Ivan Barzev <IVBarzev19@codingburgas.bg>
2021-10-18 19:45:24 +0300

    Add base ncurses.h code

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 default:
-	g++ src/main.cpp -o bin/main
+	g++ ./src/main.cpp -o ./bin/main -llibncursesw -I.\include\ncursesw -L.\lib -I.\include
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+#include <ncurses.h>
+using namespace std;
 
 int main()
 {
-    std::cout<<"Hello world";
+    initscr();
+    refresh();
+    start_color();
+    init_pair(1, COLOR_YELLOW, COLOR_MAGENTA); 
+    attron(COLOR_PAIR(1));
+    mvprintw(3, 2, "Hello world");
+    attroff(COLOR_PAIR(1));
+    refresh();
+    getch();
+    endwin();
 }
