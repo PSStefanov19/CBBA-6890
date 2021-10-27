9aca3569b2bb326598c1b88ca4af7eedf6d0cd59
PSStefanov19 <PSStefanov19@codingburgas.bg>
2021-10-27 16:43:03 +0300

    Make mazeGeneration.cpp ncurses.h compatible

--- src/mazeGeneration.cpp
+++ src/mazeGeneration.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <time.h>
+#include <ncurses.h>
 #include "mazeGeneration.h"
 using namespace std;
 
@@ -17,16 +18,15 @@ void printMaze(CELL** maze, int size, char pathSymbol)
 	{
 		for(int j = 0; j < size; j++)
 		{
-			if(maze[i][j].isVisited == true)
-			{
-				cout << pathSymbol << " ";
-			}
-			else
-			{
-				cout << "# ";
-			}
+				if(maze[i][j].isVisited == true)
+				{
+					mvprintw(i, j, " ");
+				}
+				else
+				{
+					mvprintw(i, j, "#");
+				}
 		}
-		cout<<endl;
 	}
 }
 
@@ -91,39 +91,40 @@ void moveCharacter(CELL** maze, int* cordX, int* cordY, int dir, int* unvisitedC
 		}
 }
 
-bool isAbleToMove(int dir, int size, int cordY, int cordX)
+bool canMove(int dir, int size, int cordY, int cordX)
 {
   return (dir == 0 && cordY == 1) or 
-			   (dir == 2 && cordY == size-2) or 
-			   (dir == 1 && cordX == size-2) or 
-			   (dir == 3 && cordX == 1);
+		 (dir == 2 && cordY == size-2) or 
+		 (dir == 1 && cordX == size-2) or 
+		 (dir == 3 && cordX == 1);
 }
 
 void generateMaze()
 {
 	srand(time(NULL));
+	echo();
 	int cordY, cordX = 1;
 	int allCells = 0;
-  char pathSymbol = ' ';
+	char pathSymbol = ' ';
+	int size;
+	
+	printw("Enter maze size: ");
+	scanw("%d", &size);
+	erase();
+	cordY = (rand() % size) * 2 + 1;
 
-  int size;
-  
-	cin >> size;
-
-  cordY = (rand() % size) * 2 + 1;
-
-  int exitY = (rand() % size) * 2 + 1;
+	int exitY = (rand() % size) * 2 + 1;
 
 	size = size * 2 + 1;
 
-  CELL** maze = new CELL*[size];
-  for(int i = 0; i < size; i++)
-  {
-    maze[i] = new CELL[size];
-  }
+	CELL** maze = new CELL*[size];
+	for(int i = 0; i < size; i++)
+	{
+		maze[i] = new CELL[size];
+	}
 
-  maze[cordY][cordX - 1].isVisited = true;
-  maze[exitY][size - 1].isVisited = true;
+	maze[cordY][cordX - 1].isVisited = true;
+	maze[exitY][size - 1].isVisited = true;
 
 	createWalls(maze, size, &allCells);
 
@@ -140,10 +141,10 @@ void generateMaze()
 		int dir;
 		do {
 			dir = rand()%4;
-		} while(isAbleToMove(dir, size, cordY, cordX));
+		} while(canMove(dir, size, cordY, cordX));
 		
-    moveCharacter(maze, &cordX, &cordY, dir, &unvisitedCells);
+	moveCharacter(maze, &cordX, &cordY, dir, &unvisitedCells);
 	}
 
-  printMaze(maze, size, pathSymbol);
+	printMaze(maze, size, pathSymbol);
 }
