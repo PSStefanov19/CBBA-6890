abef12d629c3df09f31052d44827a204decdf253
PSStefanov19 <PSStefanov19@codingburgas.bg>
2021-11-06 18:40:22 +0200

    Split rayCaster.cpp into functions

--- src/rayCaster.cpp
+++ src/rayCaster.cpp
@@ -14,28 +14,10 @@ bool checkIfWon(int currectPosX, int currentPosY, int exitX, int exitY)
     }
 }
 
-void rayCaster(char **maze,int size, int startY, int endY)
+void handleInput(char** maze ,float& playerX, float& playerY, float& playerAngle)
 {
-    int consoleHeight = getmaxy(stdscr);
-    int consoleWidth = getmaxx(stdscr);
-    int mapWidth = size;
-    int mapHeight = size;
-
-    float playerX = 1.0f;
-    float playerY = startY;
-    float playerAngle = 0.0f;
-    float FOV = 3.14159 / 5.5;
-    float maxRenderDistance = 10.0f;
-    float speed = 1.0f;
-
-    start_color();
-    init_pair(SKY, COLOR_BLUE, COLOR_BLUE);
-    init_pair(WALLS, COLOR_BLACK, COLOR_WHITE);
-    init_pair(FLOOR, COLOR_YELLOW, COLOR_YELLOW);
-
-    while (!(checkIfWon(int(playerX), int(playerY), size-1, endY)))
-    {
-        switch(getch())
+    float speed = 1.5f;
+    switch(getch())
         {
             case KEY_UP:
             case 'W':
@@ -70,112 +52,155 @@ void rayCaster(char **maze,int size, int startY, int endY)
                 playerAngle += speed * 0.01;
             break;
         }
+}
+
+void findWallDistance(char **maze, float rayAngle, float& distanceToWall, float playerX, float playerY, int size)
+{
+    bool hitWall = false;
+
+    float eyeX = sinf(rayAngle);
+    float eyeY = cosf(rayAngle);
+
+    while(!hitWall && distanceToWall < 10)
+    {
+        distanceToWall += 0.1f;
+
+        int testX = (int)(playerX + eyeX * distanceToWall);
+        int testY = (int)(playerY + eyeY * distanceToWall);
+
+        if(testX < 0 || testX > size || testY < 0 || testY > size)
+        {
+            hitWall = true;
+            distanceToWall = 10;
+        }
+        else 
+        {
+            if(maze[testY][testX] == '#')
+            {
+                hitWall = true;
+            }
+        }
+    }
+}
+
+void calculateCeiling(int& ceiling ,int consoleHeight, float distanceToWall)
+{
+    ceiling = (float)(consoleHeight/2.0) - consoleHeight / distanceToWall;
+}
+
+void getShade(char& shade, float distanceToWall)
+{
+    switch((int)distanceToWall)
+    {
+        case 0:
+        case 1:
+            shade = ' ';
+        break;
+        case 2:
+            shade = '.';
+        break;
+        case 3:
+            shade = ':';
+        break;
+        case 4:
+            shade = '-';
+        break;
+        case 5:
+            shade = '=';
+        break;
+        case 6:
+            shade = '+';
+        break;
+        case 7:
+            shade = '*';
+        break;
+        case 8:
+            shade = '#';
+        break;
+        case 9:
+            shade = '%';
+        break;
+        default:
+            shade = '@';
+    }
+}
+
+void drawGame(int x, int shade, int consoleHeight, int startCeiling, int startFloor, int distanceToWall)
+{
+    for(int y = 0; y <= consoleHeight; y++)
+    {
+        if(y <= startCeiling)
+        {
+            attron(COLOR_PAIR(SKY));
+            mvaddch(y, x, ' ');
+            attroff(COLOR_PAIR(SKY));
+        }
+        else if(y > startCeiling and y < startFloor and distanceToWall != 10)
+        {
+            attron(COLOR_PAIR(WALLS));
+            mvaddch(y, x, shade);
+            attroff(COLOR_PAIR(WALLS));
+        }
+        else
+        {
+            attron(COLOR_PAIR(FLOOR));
+            mvaddch(y, x, '.');
+            attroff(COLOR_PAIR(FLOOR));
+        }
+    }
+}
+
+void drawMap(char** maze, int size, int playerY, int playerX)
+{
+    for(int y = 0; y < size; y++)
+    {
+        for(int x = 0; x < size; x++)
+        {
+            mvaddch(y, x, maze[y][x]);
+        }
+    }
+
+    mvaddch(playerY, playerX, 'P');
+}
+
+void rayCaster(char **maze,int size, int startY, int endY)
+{
+    int consoleHeight = getmaxy(stdscr);
+    int consoleWidth = getmaxx(stdscr);
+
+    float playerX = 1.0f;
+    float playerY = startY;
+    float playerAngle = 0.0f;
+    float FOV = 3.14159 / 5.5;
+
+    start_color();
+    init_pair(SKY, COLOR_BLUE, COLOR_BLUE);
+    init_pair(WALLS, COLOR_BLACK, COLOR_WHITE);
+    init_pair(FLOOR, COLOR_YELLOW, COLOR_YELLOW);
+
+    while (!(checkIfWon(int(playerX), int(playerY), size-1, endY)))
+    {
+        handleInput(maze, playerX, playerY, playerAngle);
         for(int x = 0; x < consoleWidth; x++)
         {
             float rayAngle = (playerAngle - FOV/2.0) + ((float)x / (float)consoleWidth) * FOV;
 
-            float stepSize = 0.075f;
             float distanceToWall = 0.0f;
 
-            bool hitWall = false;
+            findWallDistance(maze, rayAngle, distanceToWall, playerX, playerY, size);
 
-            float eyeX = sinf(rayAngle);
-            float eyeY = cosf(rayAngle);
-
-            while(!hitWall && distanceToWall < maxRenderDistance)
-            {
-                distanceToWall += stepSize;
-
-                int testX = (int)(playerX + eyeX * distanceToWall);
-                int testY = (int)(playerY + eyeY * distanceToWall);
-
-                if(testX < 0 || testX > mapWidth || testY < 0 || testY > mapHeight)
-                {
-                    hitWall = true;
-                    distanceToWall = maxRenderDistance;
-                }
-                else 
-                {
-                    if(maze[testY][testX] == '#')
-                    {
-                        hitWall = true;
-                    }
-                }
-            }
-
-            int startCeiling = (float)(consoleHeight/2.0) - consoleHeight / ((float)distanceToWall);
+            int startCeiling;
+            calculateCeiling(startCeiling, consoleHeight, distanceToWall);
             int startFloor = consoleHeight - startCeiling;
 
             char shade = ' ';
-
-            // .:-=+*#%@
             
-            switch((int)distanceToWall)
-            {
-                case 0:
-                case 1:
-                    shade = '.';
-                break;
-                case 2:
-                    shade = '.';
-                break;
-                case 3:
-                    shade = ':';
-                break;
-                case 4:
-                    shade = '-';
-                break;
-                case 5:
-                    shade = '=';
-                break;
-                case 6:
-                    shade = '+';
-                break;
-                case 7:
-                    shade = '*';
-                break;
-                case 8:
-                    shade = '#';
-                break;
-                case 9:
-                    shade = '%';
-                break;
-                default:
-                    shade = '@';
-            }
+            getShade(shade, distanceToWall);            
 
-            for(int y = 0; y <= consoleHeight; y++)
-            {
-                if(y <= startCeiling)
-                {
-                    attron(COLOR_PAIR(1));
-                    mvaddch(y, x, ' ');
-                    attroff(COLOR_PAIR(1));
-                }
-                else if(y > startCeiling and y < startFloor)
-                {
-                    attron(COLOR_PAIR(2));
-                    mvaddch(y, x, shade);
-                    attroff(COLOR_PAIR(2));
-                }
-                else
-                {
-                    attron(COLOR_PAIR(3));
-                    mvaddch(y, x, '.');
-                    attroff(COLOR_PAIR(3));
-                }
-            }
+            drawGame(x, shade, consoleHeight, startCeiling, startFloor, distanceToWall);
         }
         
-        for(int y = 0; y < size; y++)
-        {
-            for(int x = 0; x < size; x++)
-            {
-                mvaddch(y, x, maze[y][x]);
-            }
-        }
-
-        mvaddch(playerY, playerX, 'P');
+        drawMap(maze, size, playerY, playerX);
 
         refresh();
     }
