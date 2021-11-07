9301d9930a04b678e5b8949d203b94b806ba2e9c
PSStefanov19 <PSStefanov19@codingburgas.bg>
2021-11-07 09:29:59 +0200

    Add comments to mazeGeneration.cpp

--- src/mazeGeneration.cpp
+++ src/mazeGeneration.cpp
@@ -3,6 +3,10 @@
 #include "mazeGeneration.h"
 using namespace std;
 
+/**
+ * @brief Structure that holds data for a single cell of the maze
+ * 
+ */
 struct CELL
 {
 	int y;
@@ -11,87 +15,136 @@ struct CELL
 	bool isWall = false;
 };
 
+/**
+ * @brief Transform the maze from a CELL maze to a char map
+ * 
+ * @param maze The maze made up of CELL structures
+ * @param map The map made of characters
+ * @param size The size of the maze
+ * @return char** return the maze
+ */
 char **createMaze(CELL** maze, char **map, int *size)
 {
+	//Turn CELL maze into a char map
 	for(int i = 0; i < *size; i++)
 	{
 		for(int j = 0; j < *size; j++)
 		{
-				if(maze[i][j].isVisited == true)
-				{
-					map[i][j] = ' ';
-				}
-				else
-				{
-					map[i][j] = '#';
-				}
+			//Check if cell is visited
+			if(maze[i][j].isVisited == true)
+			{
+				map[i][j] = ' ';
+			}
+			else
+			{
+				map[i][j] = '#';
+			}
 		}
 	}
 	return map;
 }
 
-void createWalls(CELL** maze, int *size, int* allCells)
+/**
+ * @brief Create walls
+ * 
+ * @param maze The maze of cell data
+ * @param size The size of the maze
+ * @param allCells Number of all cells that can be visited
+ */
+void createWalls(CELL** maze, int &size, int& allCells)
 {
-  for(int y = 0; y < *size; y++)
+	//Loop throught the whole maze
+	for(int y = 0; y < size; y++)
 	{
-		for(int x = 0; x < *size; x++)
+		for(int x = 0; x < size; x++)
 		{
+			//Check if the X or Y is a wall
 			if((x % 2 == 0) or (y % 2 == 0))
 			{
+				//Set the isWall flag to true
 				maze[y][x].isWall = true;
 			}
 			else
 			{
-				++*allCells;
+				//Add a cell to cells that can be visited
+				++allCells;
 			}
 		}
 	}
 }
 
-void moveCharacter(CELL** maze, int* cordX, int* cordY, int dir, int* unvisitedCells)
+/**
+ * @brief Function that moves the current cell to given direction
+ * 
+ * @param maze The array with the date from the maze
+ * @param cordX THe X coordinate of the current cell
+ * @param cordY The Y coordinate of the current cell
+ * @param dir The direction a cell should be moced to
+ * @param visitedCells Number of visted cells
+ */
+void moveCharacter(CELL** maze, int &cordX, int &cordY, int dir, int &visitedCells)
 {
-  switch(dir)
-		{
-			case 0:
-				if(maze[*cordY-2][*cordX].isVisited == false)
-				{
-					maze[*cordY-2][*cordX].isVisited = true;
-					maze[*cordY-1][*cordX].isVisited = true;
-					++*unvisitedCells;
-				}
-				*cordY -= 2;
-				break;
-			case 1:
-				if(maze[*cordY][*cordX+2].isVisited == false)
-				{
-					maze[*cordY][*cordX+2].isVisited = true;
-					maze[*cordY][*cordX+1].isVisited = true;
-					++*unvisitedCells++;
-				}
-				*cordX += 2;
-				break;
-			case 2:
-				if(maze[*cordY+2][*cordX].isVisited == false)
-				{
-					maze[*cordY+2][*cordX].isVisited = true;
-					maze[*cordY+1][*cordX].isVisited = true;
-					++*unvisitedCells++;
-				}
-				*cordY += 2;
-				break;
-			case 3:
-				if(maze[*cordY][*cordX-2].isVisited == false)
-				{
-					maze[*cordY][*cordX-2].isVisited = true;
-					maze[*cordY][*cordX-1].isVisited = true;
-					++*unvisitedCells++;
-				}
-				*cordX -= 2;
-				break;
-		}
+	//Switch to move current cell to given direction
+	switch(dir)
+	{
+		//Move current cell up
+		case 0:
+			//Check if upward cell is visited
+			if(maze[cordY-2][cordX].isVisited == false)
+			{
+				maze[cordY-2][cordX].isVisited = true;
+				maze[cordY-1][cordX].isVisited = true;
+				++visitedCells;
+			}
+			cordY -= 2;
+			break;
+		//Move current cell left
+		case 1:
+			//Check if the cell to the left is visited
+			if(maze[cordY][cordX+2].isVisited == false)
+			{
+				maze[cordY][cordX+2].isVisited = true;
+				maze[cordY][cordX+1].isVisited = true;
+				++visitedCells;
+			}
+			cordX += 2;
+			break;
+		//Move current cell down
+		case 2:
+			//Check if the downward cell is visited
+			if(maze[cordY+2][cordX].isVisited == false)
+			{
+				maze[cordY+2][cordX].isVisited = true;
+				maze[cordY+1][cordX].isVisited = true;
+				++visitedCells;
+			}
+			cordY += 2;
+			break;
+		//Move current cell right
+		case 3:
+			//Check if the cell to the right is visted
+			if(maze[cordY][cordX-2].isVisited == false)
+			{
+				maze[cordY][cordX-2].isVisited = true;
+				maze[cordY][cordX-1].isVisited = true;
+				++visitedCells;
+			}
+			cordX -= 2;
+			break;
+	}
 }
 
-bool canMove(int dir, int size, int cordY, int cordX)
+/**
+ * @brief A function to check whether the current cell can be moved to a certain direction
+ * 
+ * @param dir Direction the cell is moved to
+ * @param size The size of the maze
+ * @param cordY The Y coordinate of the current cell
+ * @param cordX The X coordinate of the current cell
+ * @return true Current cell cannot be moved to direction
+ * @return false Current cell can be moved to direction
+ */
+bool canNotMove(int dir, int size, int cordY, int cordX)
 {
   return (dir == 0 && cordY == 1) or 
 		 (dir == 2 && cordY == size-2) or 
@@ -101,49 +154,66 @@ bool canMove(int dir, int size, int cordY, int cordX)
 
 char **generateMaze(int mazeSize, int startY, int endY)
 {
-	echo();
-
+	//A variable holding all cells that can be visited
 	int allCells = 0;
 	
+	//The starting coordinates of the maze generation
 	int cordX = 1;
+	int cordY = 1;
+
+	//The maze with added walls
 	mazeSize = mazeSize * 2 + 1;
 
+	//An array of CELLS that holds all the data to the maze
 	CELL** mazeData = new CELL*[mazeSize];
 	for(int i = 0; i < mazeSize; i++)
 	{
 		mazeData[i] = new CELL[mazeSize];
 	}
 
+	//A character array that will hold the finished maze
 	char **maze = new char*[mazeSize];
 	for(int i = 0; i < mazeSize; i++)
 	{
 		maze[i] = new char[mazeSize];
 	}
 
+	//Make a random cell from the first column the entrance to the maze
 	mazeData[startY][0].isVisited = true;
+
+	//Make a random cell from the last column the exit of the maze
 	mazeData[endY][mazeSize - 1].isVisited = true;
 
-	createWalls(mazeData, &mazeSize, &allCells);
+	//Change the isWall flag on walls of the maze
+	createWalls(mazeData, mazeSize, allCells);
 
+	//Flag for unvisited cells
 	bool existUnvisitedCells = true;
 
-	int unvisitedCells = 0;
+	//A variable holding all cells that holds all visited cells
+	int visitedCells = 0;
 
+	//While unvisited cells exist
 	while(existUnvisitedCells)
 	{
-		if(unvisitedCells == allCells)
+		//Check if the visited cells are equal to all visitable cells
+		if(visitedCells == allCells)
 		{
 			existUnvisitedCells = false;
 		}
+		//Choose a direction to move current cell to
 		int dir;
 		do {
 			dir = rand()%4;
-		} while(canMove(dir, mazeSize, endY, cordX));
+		} while(canNotMove(dir, mazeSize, cordY, cordX));
 		
-	moveCharacter(mazeData, &cordX, &endY, dir, &unvisitedCells);
+		//Move cell to chosen direction
+		moveCharacter(mazeData, cordX, cordY, dir, visitedCells);
 	}
 
+	//Transfer data from mazeData to the character array
 	createMaze(mazeData, maze, &mazeSize);
 
+	//Return the character array
 	return maze;
 }
